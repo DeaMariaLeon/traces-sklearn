466daf57b0fd54401c6d217d78c17d470ee17f55
Dea María Léon <deamarialeon@gmail.com>
2025-05-12 16:24:56 +0200

    Removed norm factor

--- content/python_files/04_linear_model_regularization.py
+++ content/python_files/04_linear_model_regularization.py
@@ -132,7 +132,7 @@ plt.show()
 # L1 provides another regularization type. It follows this formula:
 #
 # $$
-# loss = \frac{1} {2 n\_samples} ||y - X\beta||^2_2 + \alpha  ||\beta||_1
+# loss = ||y - X\beta||^2_2 + \alpha  ||\beta||_1
 # $$
 #
 # Scikit-learn implements this as the Lasso regressor.
